f1adb8b7ade239fc019eb2ef7f47df924ecbce21
Amanda <adnamadiaz@gmail.com>
2020-07-02 00:23:26 -0400

    Primer Repositorio

--- src/main.js
+++ src/main.js
@@ -3,4 +3,35 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 
-console.log(example, data);
+const crearPlantillaPokemon = (name,image,num) => `
+        <section class="name-pokemon">
+            <img src="${image}" alt="${name}">
+            <h2 id="number-poke">#${num}</h2>
+            <h1 id="name-poke">${name}</h1>
+        </section>
+    `;
+
+
+const obtenerLugarParaImprimir = (identificador) => document.getElementById(identificador);
+
+const containerPokedex = obtenerLugarParaImprimir("pokemon-container");
+
+const imprimirEnPantalla = (pokemon) => {
+    containerPokedex.insertAdjacentHTML("beforeend", pokemon);
+};
+
+//recorremos el arreglo de todos los pokemones
+data.pokemon.forEach(pokemonAtrib => {
+    //obtener nombre
+    const pokemonName = pokemonAtrib.name;
+
+    //obtener numero
+    const pokemonNum = pokemonAtrib.num;
+
+    //obtener imagen
+    const pokemonImg = pokemonAtrib.img;
+
+    const pokemon = crearPlantillaPokemon(pokemonName,pokemonImg,pokemonNum);
+
+    imprimirEnPantalla(pokemon);
+});
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,8 +1,9 @@
-import { example, anotherExample } from '../src/data.js';
+// import { example, anotherExample } from '../src/data.js';
 
+import pokemonArray from '../src/data/pokemon/pokemon.js';
 
 describe('example', () => {
-  it('is a function', () => {
+  /* it('is a function', () => {
     expect(typeof example).toBe('function');
   });
 
@@ -19,5 +20,46 @@ describe('anotherExample', () => {
 
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
+  }); */
+
+  /* describe('testing array functions', () => {
+    it('will test the function', () => {
+      const arrayOne = [9, 6, 55, 1, 2, 3, 4, 8888];
+
+      const filtrados = arrayOne.filter(num => num >= 3);
+
+      const multiplicar = arrayOne.map(num => num * 10);
+
+      const orderArray = arrayOne.sort((num1, num2) => num2-num);
+
+      console.log(orderArray);
+
+      console.log(multiplicar);
+
+      console.log(filtrados);
+
+      expect(true).toBe(true);
+    });
+  });  */
+
+  describe('playing with pokemon', () => {
+    it('play with pokemon', () => {
+      //console.log(pokemonArray);
+      /*pokemonArray.pokemon.forEach(pokemonAtrib => {
+        console.log(pokemonAtrib.name);
+        console.log(pokemonAtrib.weaknesses);
+      });*/
+
+      //filtar por los que son del tipo poison
+      /*let pokemonPoison = pokemonArray.pokemon.filter(pokemonAtrib => pokemonAtrib.type.includes("poison"));
+      console.log(pokemonPoison.length);
+      pokemonPoison.forEach(pokemonAtrib =>{
+        console.log(pokemonAtrib.name);
+        console.log(pokemonAtrib.type);
+      });*/
+
+      
+
+    });
   });
 });
